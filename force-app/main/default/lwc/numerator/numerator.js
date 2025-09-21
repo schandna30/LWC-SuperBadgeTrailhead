76ff08e61f471e85f19703b888f5a60b2a8d258a
Simran <schandna308@gmail.com>
2025-09-21 23:22:04 +0530

    Changes

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,29 +1,38 @@
-import { LightningElement ,api} from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-   @api counter =0
     _currentCount = 0;
-  priorCount = 0;
+    priorCount = 0;
+
     @api
-  maximizeCounter() {
-    this.counter += 1000000;
-  }
-  get counter() {
-    return this._currentCount;
-  }
-  set counter(value) {
-    this.priorCount = this._currentCount;
-    this._currentCount = value;
-  }
-    handleIncrement(){
+    maximizeCounter() {
+        this.counter += 1000000;
+    }
+
+    @api
+    get counter() {
+        return this._currentCount;
+    }
+
+    set counter(value) {
+        this.priorCount = this._currentCount;
+        this._currentCount = value;
+    }
+
+    handleIncrement() {
         this.counter++;
     }
-      handleDecrement(){
+
+    handleDecrement() {
         this.counter--;
     }
-    handleMultiply(event){
-        const factor= event.detail;
-        this.counter*=factor;
 
+    handleMultiply(event) {
+        const factor = event.detail;
+        this.counter *= factor;
     }
 }
+// //Client ID:
+// 21cd1316de324574b9289d86f31c6c27 
+// Client Secret:
+// 8A87c9bD4a81461EB82936E402d28363
